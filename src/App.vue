d4f1e1eed6bc3aa6ef777b1dc791ee5c6889c693
vuewebdev <rybniko@gmail.com>
2023-01-01 10:59:56 +0200

    Style & LICENSE.md

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,9 @@ import { RouterView } from "vue-router";
 
 <template>
   <div class="flex items-center justify-center h-screen bg-slate-200">
-    <div class="flex-column h-[20vh] text-center [&_p]:my-[5vh]">
-      {{ $t("header.name") }}
+    <div class="flex-column h-[40vh] text-center [&_p]:mt-[5vh]">
+      <img class="mx-auto" src="./assets/logo.svg" />
+      <p>{{ $t("header.name") }}</p>
       <RouterView />
     </div>
   </div>
